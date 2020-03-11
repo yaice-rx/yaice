6f9c9a6170f2bb1dc8d349cbb9001c4813c0eb0b
yaice <yaice.rx@qq.com>
2020-03-11 18:53:03 +0800

    update

--- network/tcp/Conn.go
+++ network/tcp/Conn.go
@@ -3,7 +3,7 @@ package tcp
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
+	"errors"
 	"github.com/golang/protobuf/proto"
 	"github.com/satori/go.uuid"
 	"github.com/yaice-rx/yaice/log"
@@ -13,6 +13,7 @@ import (
 	"go.uber.org/zap"
 	"io"
 	"net"
+	"sync/atomic"
 	"time"
 )
 
@@ -23,18 +24,27 @@ type Conn struct {
 	receiveQueue chan network.TransitData
 	sendQueue    chan []byte
 	stopChan     chan bool
+	isClosed     bool
 	times        int64
+	reconnCount  uint32
+	reconnTime   int64
 	data         interface{}
 }
+
+const TOKEN_CHECK_MAX_TRY_NUM = 60
+const TOKEN_CHECK_MAX_WAIT_TIME = 5
 
 func NewConn(conn *net.TCPConn, pkg network.IPacket) network.IConn {
 	return &Conn{
 		guid:         uuid.NewV4().String(),
 		conn:         conn,
 		pkg:          pkg,
-		receiveQueue: make(chan network.TransitData),
-		sendQueue:    make(chan []byte),
+		isClosed:     false,
+		receiveQueue: make(chan network.TransitData, 10),
+		sendQueue:    make(chan []byte, 10),
 		stopChan:     make(chan bool),
+		reconnCount:  0,
+		reconnTime:   0,
 		times:        time.Now().Unix(),
 	}
 }
@@ -49,7 +59,7 @@ func (c *Conn) readThread() {
 		headData := make([]byte, c.pkg.GetHeadLen())
 		_, err := io.ReadFull(c.conn, headData) //ReadFull 会把msg填充满为止
 		if err != nil {
-			fmt.Println("read head error")
+			log.AppLogger.Debug("network io read data err:" + err.Error())
 			break
 		}
 		//强制规定网络数据包头4位必须是网络的长度
@@ -57,7 +67,7 @@ func (c *Conn) readThread() {
 		headerBuff := bytes.NewReader(headData)
 		msg := &headerMsg{}
 		if err := binary.Read(headerBuff, binary.BigEndian, &msg.DataLen); err != nil {
-			fmt.Println("server unpack err:", err)
+			log.AppLogger.Debug("server unpack err:" + err.Error())
 			break
 		}
 		if msg.DataLen > 0 {
@@ -66,11 +76,11 @@ func (c *Conn) readThread() {
 			//根据dataLen从io中读取字节流
 			_, err := io.ReadFull(c.conn, contentData)
 			if err != nil {
-				fmt.Println("server unpack data err:", err)
+				log.AppLogger.Debug("network io read data err:" + err.Error())
 				break
 			}
 			//解压网络数据包
-			msgData,err :=  c.pkg.Unpack(contentData)
+			msgData, err := c.pkg.Unpack(contentData)
 			//写入通道数据
 			c.receiveQueue <- network.TransitData{
 				MsgId: msgData.GetMsgId(),
@@ -85,13 +95,25 @@ func (c *Conn) writeThread() {
 		select {
 		case data, state := <-c.sendQueue:
 			if state {
+			LOOP:
 				_, err := c.conn.Write(data)
 				if err != nil {
-					//首先判断 发送多次，依然不能连接服务器，就此直接断开
-					//todo
+					time.Sleep(800 * time.Microsecond)
+					if atomic.LoadUint32(&c.reconnCount) >= TOKEN_CHECK_MAX_TRY_NUM {
+						log.AppLogger.Info("reconnect faild")
+						atomic.SwapUint32(&c.reconnCount, 0)
+						c.Close()
+					} else {
+						log.AppLogger.Info("reconnecting ...")
+						atomic.AddUint32(&c.reconnCount, 1)
+						goto LOOP
+					}
+				} else {
+					log.AppLogger.Info("reconnect  success ...")
+					//连通后，重置为0
+					atomic.SwapUint32(&c.reconnCount, 0)
 				}
-			} else {
-				//todo  读取数据出错
+				break
 			}
 		}
 	}
@@ -105,12 +127,18 @@ func (c *Conn) Send(message proto.Message) error {
 		log.AppLogger.Error("发送消息时，序列化失败 : "+err.Error(), zap.Int32("MessageId", protoId))
 		return err
 	}
+	if c.isClosed == true {
+		return errors.New("Connection closed when send msg")
+	}
 	c.sendQueue <- c.pkg.Pack(network.TransitData{protoId, data})
 	return nil
 }
 
 //发送组装好的协议，但是加密始终是在组装包的时候完成加密功能
 func (c *Conn) SendByte(message []byte) error {
+	if c.isClosed == true {
+		return errors.New("Connection closed when send msg")
+	}
 	c.sendQueue <- message
 	return nil
 }
@@ -127,22 +155,28 @@ func (c *Conn) Start() {
 			select {
 			//读取网络数据
 			case data := <-c.receiveQueue:
-				if data.MsgId != 0  {
+				if data.MsgId != 0 {
 					router.RouterMgr.ExecRouterFunc(c, data)
 				}
 				break
 			//关闭Conn连接
-			case data := <-c.stopChan:
-				if data {
-					//todo
-				}
-				break
+			case <-c.stopChan:
+				return
 			}
 		}
 	}()
 }
 
 func (c *Conn) Close() {
+	//如果当前链接已经关闭
+	if c.isClosed == true {
+		return
+	}
+	c.stopChan <- true
+	c.isClosed = true
+	c.conn.Close()
+	close(c.receiveQueue)
+	close(c.sendQueue)
 }
 
 func (c *Conn) GetTimes() int64 {
@@ -157,7 +191,6 @@ func (c *Conn) SetData(data interface{}) {
 	c.data = data
 }
 
-
-func (c *Conn)GetConn()interface{}{
+func (c *Conn) GetConn() interface{} {
 	return c.conn
 }
