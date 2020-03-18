ade432337d4e5c2dcdc41b53b9cb130148f4faa9
yaice <yaice.rx@qq.com>
2020-03-18 14:01:23 +0800

    update 添加扩展方法

--- network/IPackate.go
+++ network/IPackate.go
@@ -3,6 +3,5 @@ package network
 type IPacket interface {
 	GetHeadLen() uint32
 	Pack(TransitData) []byte
-	Unpack(binaryData []byte) (IMessage, error)
+	Unpack(binaryData []byte) (IMessage, error, func(conn IConn))
 }
-
--- network/tcp/Packate.go
+++ network/tcp/Packate.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	ConstMsgLength    = 4 //消息长度
-	ConstMsgIdLen	= 4
+	ConstMsgLength = 4 //消息长度
+	ConstMsgIdLen  = 4
 )
 
 type packet struct {
@@ -19,30 +19,31 @@ func NewPacket() network.IPacket {
 	return &packet{}
 }
 
-func(dp *packet) GetHeadLen() uint32 {
+func (dp *packet) GetHeadLen() uint32 {
 	//DataLen uint32(4字节)
 	return ConstMsgLength
 }
+
 //封包
 func (dp *packet) Pack(msg network.TransitData) []byte {
-	msgLength := int32(len(msg.Data)+ConstMsgIdLen)
+	msgLength := int32(len(msg.Data) + ConstMsgIdLen)
 	dataLen := utils.IntToBytes(msgLength)
-	dataId :=  utils.IntToBytes(msg.MsgId)
-	return append(append(dataLen,dataId ...), msg.Data...)
+	dataId := utils.IntToBytes(msg.MsgId)
+	return append(append(dataLen, dataId...), msg.Data...)
 }
 
 //解包
-func (dp *packet) Unpack(binaryData []byte) (network.IMessage, error) {
+func (dp *packet) Unpack(binaryData []byte) (network.IMessage, error, func(conn network.IConn)) {
 	//创建一个从输入二进制数据的ioReader
 	dataBuff := bytes.NewReader(binaryData)
 	//只解压head的信息，得到dataLen和msgID
 	msg := &Message{}
 	//读msgID
 	if err := binary.Read(dataBuff, binary.BigEndian, &msg.ID); err != nil {
-		return nil, err
+		return nil, err, nil
 	}
 	//读msgID
 	msg.Data = binaryData[ConstMsgIdLen:]
 	//这里只需要把head的数据拆包出来就可以了，然后再通过head的长度，再从conn读取一次数据
-	return msg, nil
+	return msg, nil, nil
 }
