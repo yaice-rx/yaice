97ae5e22e61bf6616ea067218d43bf1dd7df41ee
yaice <yaice.rx@qq.com>
2022-05-07 13:59:13 +0800

    uodate

--- network/Options.go
+++ network/Options.go
@@ -1,7 +1,5 @@
 package network
 
-import "sync/atomic"
-
 type ServeType int32
 
 const (
@@ -11,21 +9,20 @@ const (
 
 type IOptions interface {
 	GetMaxRetires() int32
-	GetTimesTotal() *int64
-	GetHttpCreateTime() int64
+	GetTimeMills() int64
 }
 
 type Options struct {
 	maxRetries int32
-	timesTotal *int64
-	httpCreateTime int64
+	timeMills int64
+	data interface{}
 }
 
-func WithMax(maxRetries int32,timesTotal *int64,httpCreateTime int64) IOptions {
+func WithMax(maxRetries int32,timeMills int64,data interface{}) IOptions {
 	return &Options{
 		maxRetries: maxRetries,
-		timesTotal:timesTotal,
-		httpCreateTime:httpCreateTime,
+		timeMills:timeMills,
+		data:data,
 	}
 }
 
@@ -33,14 +30,10 @@ func (o *Options) GetMaxRetires() int32 {
 	return o.maxRetries
 }
 
-func (o *Options) GetTimesTotal() *int64 {
-	return o.timesTotal
+func (o *Options) GetTimeMills() int64 {
+	return o.timeMills
 }
 
-func (o *Options) AddTimesTotal(times int64) int64 {
-	return atomic.AddInt64(o.timesTotal,times)
-}
-
-func (o *Options) GetHttpCreateTime() int64 {
-	return o.httpCreateTime
+func (o *Options) GetData() interface{} {
+	return o.data
 }
---
+++ network/network.go
@@ -0,0 +1,5 @@
+package network
+
+type INetwork interface {
+	Close()
+}
